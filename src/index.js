5d6e5f87a8e072e54841bcf949813f3e85368c1c
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-10-02 21:13:05 +0300

    hw completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const countryInput = document.querySelector('input');
+export const countryList = document.querySelector('ul');
+export const coutryData = document.querySelector('div');
+
+countryInput.addEventListener(
+  'input',
+  debounce(function () {
+    if (countryInput.value !== '') {
+      fetchCountries(countryInput.value.trim());
+    }
+  }, DEBOUNCE_DELAY)
+);
